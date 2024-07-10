b33dcc0e17aedd4c8e96ed102c522a952be1cf12
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-07-10 20:10:46 +0530

    setup of axios instance

--- Frontened/src/main.jsx
+++ Frontened/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './Redux/Slices/Store.js'
+import store from './Redux/Store.js'
 
 
 
