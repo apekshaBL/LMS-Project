1be6b030e809af5c8f87e2de4d30deb8be7809d0
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-07-10 19:38:14 +0530

    added basic react redux setup using redux toolkit

--- Frontened/src/main.jsx
+++ Frontened/src/main.jsx
@@ -3,15 +3,20 @@ import App from './App.jsx'
 import './index.css'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './Redux/Slices/Store.js'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <BrowserRouter>
+<Provider store={store}>
+<BrowserRouter>
      <App />
-     <Toaster/>
-      {/* From react-hot-toast we get toaster element only we have to add it so that it is enable to all files*/}
+     <Toaster/>{/* From react-hot-toast we get toaster element only we have to add it so that it is enable to all files*/}
+      
    </BrowserRouter>
+  </Provider>
+   
   
   
 )
